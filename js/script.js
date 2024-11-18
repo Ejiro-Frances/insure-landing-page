95e53df60dd766f95c4e76adb4ad9527d0ef62ae
Ejiro-Frances <francesejiro@yahoo.com>
2024-11-18 04:15:53 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //get hamburger menu and add event listener
 const mobileMenu = document.querySelector("#hamburger-icon");
 const mobileLinks = document.querySelector(".nav-links");
+const body = document.body;
 
 mobileMenu.addEventListener("click", () => {
   mobileLinks.classList.toggle("activeMobile");
@@ -10,4 +11,8 @@ mobileMenu.addEventListener("click", () => {
   } else {
     mobileMenu.src = "./images/icon-hamburger.svg";
   }
+
+  body.style.overflow = mobileLinks.classList.contains("activeMobile")
+    ? "hidden"
+    : "";
 });
