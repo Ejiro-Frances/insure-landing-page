e449aa84a43a7cd6fec3a7fffec848a6b4d5f233
Ejiro-Frances <francesejiro@yahoo.com>
2024-11-18 04:01:35 +0100

    update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const mobileLinks = document.querySelector(".nav-links");
 
 mobileMenu.addEventListener("click", () => {
   mobileLinks.classList.toggle("activeMobile");
+
   if (mobileLinks.classList.contains("activeMobile")) {
     mobileMenu.src = "./images/icon-close.svg";
   } else {
